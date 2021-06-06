f13978bd0e4bf91ea13412335422ecfafe174692
Lewis <louislaizer4@gmail.com>
2021-06-06 14:16:31 +0300

    :hammer:
    Stable

--- src/index.js
+++ src/index.js
@@ -12,6 +12,8 @@ app.use(cors())
 
 app.use(function (req, res, next) {
     res.header("Access-Control-Allow-Origin", "*")
+    res.header("Access-Control-Allow-Methods", "DELETE, POST, GET, OPTIONS")
+    res.header("Access-Control-Allow-Headers", "Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With")
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept")
     next()
 })
